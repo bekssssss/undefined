a2a654f6b1401549651498df61269fccff82387b
bekssssss <porshen.2003@mail.ru>
2024-07-02 13:52:36 +0600

    lox

---
+++ script.js
@@ -0,0 +1,29 @@
+// let a = 4
+// let b = 55
+// console.log(a+b)
+// console.log(a*b)
+// console.log(a/b)
+// console.log(a%b)
+
+
+
+// let c = 'hello world'
+// let d = 'bek japarkulov'
+// console.log(c, d)
+//
+// let e = 5
+// let f = 'E'
+//
+// console.log(f * e)
+
+let a = 5
+let b = 5
+console.log(a +b)
+
+let c = 2
+let d = undefined
+console.log(c == d)
+
+
+// # 2 - eto chislo, a undefined on na chille
+
